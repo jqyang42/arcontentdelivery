c4036026fb27be51abfc97148ee51cc38c66dfba
Jessica Yang <jessica.yang@duke.edu>
2021-12-01 02:30:59 -0500

    refactoring

--- latency.py
+++ latency.py
@@ -4,32 +4,15 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
-light_times = []
-dark_times = []
 file_model_Mb = {
-    'Default 50 Trials' : 148.00352,
-    'fish_100kTrials' : 45.472128,
-    'fish_200kTrials' : 64.0528,
-    'fish_400kTrials' : 148.00352,
-    'fish_2kTrials' : 24.807808,
-    'fish_20kTrials' : 29.271456,
-    'question_3' : 0
+    'Default' : 148.00352,
+    'Fish 100k' : 45.472128,
+    'Fish 200k' : 64.0528,
+    'Fish 400k' : 148.00352,
+    'Fish 2k' : 24.807808,
+    'Fish 20k' : 29.271456,
+    'Busy Network' : 0
 }
-
-title = 'question_3'
-for filename in glob.glob(f'trials/{folder}/*.txt'):
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            values = line.split(':')
-            if 'light' in values[0]:
-                light_times.append(float(values[3]))
-            else:
-                dark_times.append(float(values[3]))
-
-light_times = np.array(light_times)
-dark_times = np.array(dark_times)
-times = pd.DataFrame(np.hstack((light_times[:,None], dark_times[:,None])), columns=['light', 'dark'])
 
 def latency_calculations(dataset, title):
     for context in ['light', 'dark']:
@@ -38,7 +21,7 @@ def latency_calculations(dataset, title):
         with open(f'calculations/{title}_{context}.txt', 'w') as f:
             f.write(f'mean: {mean}\n')
             f.write(f'std: {std}\n')
-            f.write(f'throughput: {file_model_Mb[folder] / mean}\n')
+            f.write(f'throughput: {file_model_Mb[title] / mean}\n')
 
     ax = sns.histplot(data=dataset, element='step')
     graph_title = f'{title} Model Latency'
@@ -46,4 +29,21 @@ def latency_calculations(dataset, title):
     plt.savefig(f'graphs/{graph_title}.png')
     plt.clf()
 
-latency_calculations(times)
+for folder in glob.glob(f'trials/*'):
+    light_times = []
+    dark_times = []
+
+    for filename in glob.glob(f'{folder}/*.txt'):
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                values = line.split(':')
+                if 'light' in values[0]:
+                    light_times.append(float(values[3]))
+                else:
+                    dark_times.append(float(values[3]))
+
+    light_times_np = np.array(light_times)
+    dark_times_np = np.array(dark_times)
+    times = pd.DataFrame(np.hstack((light_times_np[:,None], dark_times_np[:,None])), columns=['light', 'dark'])
+    latency_calculations(times, folder[7:])
